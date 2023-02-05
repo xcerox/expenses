8da1b1eb508e94af5c29485239956ada3ee80376
Jairo Reyes <ing.jairoreyes@gmail.com>
2023-02-05 10:23:13 +0100

    Updates

---
+++ precache-manifest.850ea3fa95c98e3ed7d142f7e2fa8590.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c4d5161677aecdac8c2",
+    "url": "/expenses/static/css/main.5d11ef91.chunk.css"
+  },
+  {
+    "revision": "1c4d5161677aecdac8c2",
+    "url": "/expenses/static/js/main.1c4d5161.chunk.js"
+  },
+  {
+    "revision": "e0e6fef0b605b46ee9db",
+    "url": "/expenses/static/js/1.e0e6fef0.chunk.js"
+  },
+  {
+    "revision": "540cc011675d24d96f31",
+    "url": "/expenses/static/js/runtime~main.540cc011.js"
+  },
+  {
+    "revision": "b4f4c4c652a62586d86619d42a16da0f",
+    "url": "/expenses/index.html"
+  }
+];
